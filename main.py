895bfbb8e22fe495233ff6018905e50989abd029
eskayjj <shawnkoh25@gmail.com>
2023-01-27 16:05:43 +0800

    ML API Functional, HTML requires patching

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import uvicorn
-import pandas as pd
-import numpy as np
-import torch
-import torch.nn as nn
 from fastapi import FastAPI, Request, File, UploadFile, Form
 from pydantic import BaseModel
 from starlette.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-from typing import List
 from predictor import prediction
-from PIL import Image
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -20,19 +14,14 @@ class Number(BaseModel):
 
 @app.get('/') #double decorator
 @app.get('/home', response_class=HTMLResponse)
-async def homepage(request: Request):
+async def homepage(request: Request, title: str):
     return templates.TemplateResponse('home.html', {"request": request})
 
 @app.post("/predict")
-def predict(request: Request):
-    result = prediction()
-    return templates.TemplateResponse('predict.html', context={'request': request, 'result': result})
-
-@app.post("/uploadfiles/")
-async def create_upload_files(
-    files: List[UploadFile] = File(description="Multiple files as UploadFile"),
-):
-    return {"filenames": [file.filename for file in files]}
+async def predict(request: Request, title: str, files: UploadFile = File(...)):
+    contents = await files.read()
+    result = prediction(contents)
+    return templates.TemplateResponse('predict.html', {'request': request, 'result': result})
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
