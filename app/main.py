ac1ade2c017ec84e7643d8077084877ee766a003
eskayjj <shawnkoh25@gmail.com>
2023-02-21 13:41:50 +0800

    Change host IP

--- app/main.py
+++ app/main.py
@@ -73,4 +73,4 @@ async def train(file:train):
     return {"trained": trained}
     
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8080, reload=True)
