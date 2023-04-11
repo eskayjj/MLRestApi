c1653bd5f21db5b68f8bfac02b1bcebcc8f9816a
eskayjj <shawnkoh25@gmail.com>
2023-04-12 03:33:48 +0800

    Test Case edited

--- test/test.py
+++ test/test.py
@@ -1,24 +1,33 @@
 import requests
 import traceback
+import json
 
-url = "http://127.0.0.1:8080" #need domain
+url = "http://35.239.210.99:8080/predict/640704fee9e9c81d970e5096" #need domain
 
 #Comment/Uncomment this for use of /predict/ API
-new_picture = {
-    "filename": 'C:/Users/User/AStar Intern/Prototype/RESTApi/Dataset/AntBee/train/ants/Ant_1.jpg'  #serialise this address
-}
+# data = open('C:/Users/User/AStar Intern/Prototype/RESTApi/Dataset/AntBee/train/ants/Ant_1.jpg', 'rb')
+
+# new_picture = {
+#     #"id": '640704fee9e9c81d970e5096',
+#     "file": data #serialise this address
+# }
 # files = {'file': ('foo.txt', open('./foo.txt', 'rb'))}
 # response = requests.post('http://127.0.0.1:8000/file', files=files)
 # print(response)
 # print(response.json())
 
 try:
-    response = requests.post(f"{url}/predict", json=new_picture)
+    with open('C:/Users/User/AStar Intern/Prototype/RESTApi/Dataset/AntBee/train/ants/Ant_1.jpg', 'rb') as f:
+        files = {'file': ('Ant_1.jpg', f, 'image/jpeg')}
+        response = requests.post(url=url, files=files)
 except:
     traceback.print_exc()
     print(Exception)
 finally:
-    print(response.json())
+    print(response.text)
+    response_json = json.loads(response.text)
+    value = response_json['result']
+    print(value)
 
 #Comment/Uncomment this for use of /train/ API
 #newFile = {"filename": 'C:/Users/User/AStar Intern/Prototype/RESTApi/Dataset/AntBee.zip'}
